4dec041852ff6e73483ae58690ba8d1322d76e84
zyagaimooisii <127655232+zyagaimooisii@users.noreply.github.com>
2024-07-03 19:09:31 +0900

    Update views.py

--- templates/testapp/views.py
+++ templates/testapp/views.py
@@ -5,3 +5,6 @@ from testapp import app
 @app.route('/')
 def index():
     return render_template('testapp/index.html')
+    @app.route('/sampleform')
+def sample_form():
+    return render_template('testapp/sampleform.html')
